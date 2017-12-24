ea56c60d9e0f3a36f5116c5bfb4ceb254b0cc6d8
Puneet Bhojwani <puneet.bhojwani@synapse.com>
2017-12-24 15:14:30 +0000

    Added to Git Repo

---
+++ src/test/java/com/microservice/cloudnative/zuulreverseproxy/ZuulreverseproxyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.microservice.cloudnative.zuulreverseproxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZuulreverseproxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
